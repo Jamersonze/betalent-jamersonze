6cf4f1b3c608c63f1c9fdc1f0eda9c04611ac599
Jamersonze <jamersonze54@gmail.com>
2025-03-07 19:57:55 -0300

    feat: implement expandable rows in EmployeeRowMobile for detailed employee information

--- frontend/src/components/Employee/Row/Mobile.tsx
+++ frontend/src/components/Employee/Row/Mobile.tsx
@@ -17,19 +17,44 @@ const EmployeeRowMobile: React.FC<EmployeeRowProps> = ({ employee }) => {
     };
     
     return (
-        <tr>
-            <td>
-                <picture>
-                    <source srcSet={employee.image} type="image/webp" />
-                    <source srcSet={employee.image} type="image/png" />
-                    <img width={50} src={employee.image} alt="Employee Image" />
-                </picture>
-            </td>
-            <td>{employee.name}</td>
-            <td>
-                <img src="/Employee/Table/Row/charm_chevron-down.svg" alt="Abrir " />
-            </td>
-        </tr>
+        <>
+            <tr onClick={handleExpand}>
+                <td>
+                    <picture>
+                        <source srcSet={employee.image} type="image/webp" />
+                        <source srcSet={employee.image} type="image/png" />
+                        <img width={50} src={employee.image} alt="Employee Image" />
+                    </picture>
+                </td>
+                <td>{employee.name}</td>
+                <td>
+                    {isExpanded ? (
+                        <img src="/Employee/Table/Row/charm_chevron-up.svg" alt="Abrir " />
+                    ) : (
+                        <img src="/Employee/Table/Row/charm_chevron-down.svg" alt="Abrir " />
+                    )}
+                </td>
+            </tr>
+            {isExpanded && (
+                <tr>
+                    <td colSpan={3}>
+                        <section className='employee-info'>
+                            <strong>Cargo:</strong>
+                            <p>{employee.job}</p>
+                            
+                        </section>
+                        <section className='employee-info'>
+                            <strong>Data de Admissão: </strong>
+                            {formatAdmissionDate(employee.admission_date)}
+                        </section>
+                        <section className='employee-info'>
+                            <strong>Telefone: </strong>
+                            {formatPhoneNumber(employee.phone)}
+                        </section>
+                    </td>
+                </tr>
+            )}
+        </>
     );
 };
 
